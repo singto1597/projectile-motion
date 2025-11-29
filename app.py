83071ca553f6a92c93eb7f09e4d5a1a9844670ca
singto1597 <singto1597@gmail.com>
2025-11-29 18:22:59 +0700

    add pygame interface

--- app.py
+++ app.py
@@ -1,34 +1,92 @@
+import pygame
 import math
 
-g = 9.81 
-dt = 0.1 
+pygame.init()
 
-x = 0
-y = 0
-speed = 50
-angle = 45
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Real-time Projectile Motion")
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+g = 9.81    # m / s2
+speed = 20  # m / s
+angle = 90  # องศา
+
+
+PPM = 20  # Pixels Per Meter 
+
+start_x = 0
+start_y = 0
+
+x_m = 0
+y_m = start_y / PPM 
 
 theta = math.radians(angle)
 vx = speed * math.cos(theta)
 vy = speed * math.sin(theta)
 
-t = 0
-print(f"เวลา: {t:.1f}s -> ตำแหน่ง: ({x:.2f}, {y:.2f})")
+trajectory = []
+running = True
+clock = pygame.time.Clock()
 
-while y >= 0:
-    x = x + (vx * dt)
-    y = y + (vy * dt)
-    
-    # v = a x t
-    # dv = -g x t
-    # v = v + dv
-    # v = v + (-g x t)
-    # v = v - g x t
-    vy = vy - (g * dt)
-    
-    t += dt
-    
-    if y >= 0:
-        print(f"เวลา: {t:.1f}s -> ตำแหน่ง: ({x:.2f}, {y:.2f})")
+time = 0
+
+while running:
+    dt = clock.tick(60) / 1000.0 
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    if y_m >= 0:
+        x_m = x_m + (vx * dt)
+        y_m = y_m + (vy * dt)
+
+        # v = a x t
+        # dv = -g x t
+        # v = v + dv
+        # v = v + (-g x t)
+        # v = v - g x t
+        vy = vy - (g * dt)
     else:
-        print("บอลตกพื้นแล้ว")
+        y_m = 0
+        vx = 0
+        vy = 0
+
+    screen_x = start_x + (x_m * PPM)
+    
+
+    screen_y = HEIGHT - 10 - (y_m * PPM)
+
+    if vx != 0: 
+        trajectory.append((screen_x, screen_y))
+        time += dt
+
+    screen.fill(WHITE)
+
+    pygame.draw.line(screen, BLACK, (0, HEIGHT-10), (WIDTH, HEIGHT-10), 2)
+
+    if len(trajectory) > 1:
+        pygame.draw.lines(screen, BLUE, False, trajectory, 2)
+
+    pygame.draw.circle(screen, RED, (int(screen_x), int(screen_y)), 10)
+    
+    font = pygame.font.SysFont(None, 24)
+    img = font.render(f"Time: {time:.4f}s", True, BLACK)
+    screen.blit(img, (10, 10))
+    img = font.render(f"Height: {y_m:.2f}m", True, BLACK)
+    screen.blit(img, (10, 30))
+    img = font.render(f"Distance: {x_m:.2f}m", True, BLACK)
+    screen.blit(img, (10, 50))
+    img = font.render(f"Y Velocity: {vy:.2f}m/s", True, BLACK)
+    screen.blit(img, (10, 70))
+
+    pygame.display.flip()
+    
+
+pygame.quit()
